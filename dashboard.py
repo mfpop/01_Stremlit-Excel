c55db143a624176cdb41215ed2e0bff24bb6d7e3
mihai <mihaipop@outlook.com>
2025-01-14 14:30:06 -0800

    2

--- dashboard.py
+++ dashboard.py
@@ -5,11 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_title='Dashboard', page_icon=':bar_chart:', layout='wide')
 
-st.write("""sumary_line
-
-Keyword arguments:
-argument -- description
-Return: return_description
+st.write("""#Dashboard
 """
 )
 
