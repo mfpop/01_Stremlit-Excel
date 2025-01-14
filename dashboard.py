4b8abac6f9214a6c42468e2bffe8ee1851013df4
mihai <mihaipop@outlook.com>
2025-01-14 15:48:53 -0800

    2

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as px
 
-#df=pd.read_excel(    'data.xlsx',    sheet_name='Sheet1',    header=0)
+df=pd.read_excel('Etiquetas.xlsx')
 
 st.set_page_config(page_title='Dashboard', page_icon=':bar_chart:', layout='wide')
  
@@ -12,5 +13,3 @@ st.write("""   ### Efficiency    """)
 #create sidebar table with filters and data columns
 
 st.sidebar.title('Filter by:') 
-
-category = st.sidebar.multiselect('Category', ['A', 'B', 'C'], default=['A', 'B', 'C'])
