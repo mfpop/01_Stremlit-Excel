9625be76c3993920c3a7fac156489e2478969fcf
mihai <mihaipop@outlook.com>
2025-01-14 14:38:16 -0800

    2

--- dashboard.py
+++ dashboard.py
@@ -5,8 +5,10 @@ import streamlit as st
 
 st.set_page_config(page_title='Dashboard', page_icon=':bar_chart:', layout='wide')
 
-st.title("#Dashboard")
-
+st.title('Dashboard')
+         
 #create sidebar table with filters and data columns
+
 st.sidebar.title('Filter by:') 
+
 category = st.sidebar.multiselect('Category', ['A', 'B', 'C'], default=['A', 'B', 'C'])
